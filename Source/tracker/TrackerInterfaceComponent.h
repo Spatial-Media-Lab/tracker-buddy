0b4b855c2c60f0cefeaed18b2685b2c4f45a1685
Daniel Rudrich <mail@danielrudrich.de>
2021-05-11 22:11:34 +0200

    adds colourpicker and adds name to layout

--- Source/tracker/TrackerInterfaceComponent.h
+++ Source/tracker/TrackerInterfaceComponent.h
@@ -15,14 +15,14 @@ public:
     void paint (juce::Graphics& g) override
     {
         auto bounds = getLocalBounds().toFloat();
-        bounds.reduce (2, 2);
+        bounds.reduce (3, 3);
 
         g.setColour (connected ? juce::Colours::limegreen : juce::Colours::red);
-        g.drawEllipse (bounds, 1.0f);
+        g.drawEllipse (bounds, 2.0f);
 
         if (active)
         {
-            bounds.reduce (2, 2);
+            bounds.reduce (3, 3);
             g.fillEllipse (bounds);
         }
     }
@@ -54,30 +54,70 @@ private:
 
 class TrackerInterfaceComponent : public juce::Component, public TrackerInterface::Listener, public juce::Timer
 {
+    class ColourPicker : public juce::Component, private juce::ChangeListener
+    {
+    public:
+        ColourPicker (TrackerInterface& tracker) : t (tracker) {}
+        ~ColourPicker() = default;
+
+        void paint (juce::Graphics& g) override
+        {
+            g.setColour (currentColour);
+            g.fillRoundedRectangle (getLocalBounds().toFloat(), 4.0f);
+        }
+
+        void mouseUp (const juce::MouseEvent& event) override
+        {
+            auto colourSelector = std::make_unique<juce::ColourSelector> (juce::ColourSelector::showColourspace);
+            colourSelector->setName ("LED Color");
+            colourSelector->setCurrentColour (currentColour);
+            colourSelector->addChangeListener (this);
+            colourSelector->setColour (juce::ColourSelector::backgroundColourId, juce::Colours::transparentBlack);
+            colourSelector->setSize (250, 250);
+
+            juce::CallOutBox::launchAsynchronously (std::move (colourSelector), getScreenBounds(), nullptr);
+        }
+
+        void changeListenerCallback (juce::ChangeBroadcaster* source) override
+        {
+            if (juce::ColourSelector* cs = dynamic_cast<juce::ColourSelector*> (source))
+            {
+                currentColour = cs->getCurrentColour();
+
+                t.setHue (currentColour.getHue() * 360);
+                t.setSaturation (currentColour.getSaturation());
+                t.setBrightness (currentColour.getBrightness());
+                repaint();
+            }
+        }
+
+        void setHSV (float hue, float saturation, float brightness)
+        {
+            currentColour = juce::Colour::fromHSV (hue, saturation, brightness, 1.0f);
+            repaint();
+        }
+
+        juce::Colour getColour() const
+        {
+            return currentColour;
+        }
+
+    private:
+        TrackerInterface& t;
+        juce::Colour currentColour;
+    };
+
 public:
     static int getIdealHeight() { return 80; };
 
     TrackerInterfaceComponent (TrackerInterface& interface) :
-    trackerInterface (interface)
+    trackerInterface (interface),
+    colourPicker (interface)
     {
         activity.setConnected (trackerInterface.isDeviceOpen());
         addAndMakeVisible (activity);
 
-        hueSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 20);
-        hueSlider.setRange (0.0, 360.0);
-        hueSlider.onValueChange = [&] () { trackerInterface.setHue (hueSlider.getValue()); };
-        addAndMakeVisible (hueSlider);
-
-        satSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 20);
-        satSlider.setRange (0.0, 1.0);
-        satSlider.onValueChange = [&] () { trackerInterface.setSaturation (satSlider.getValue()); };
-        addAndMakeVisible (satSlider);
-
-        brightSlider.setTextBoxStyle (juce::Slider::TextBoxBelow, false, 80, 20);
-        brightSlider.setRange (0.0, 1.0);
-        brightSlider.onValueChange = [&] () { trackerInterface.setBrightness (brightSlider.getValue()); };
-        addAndMakeVisible (brightSlider);
-
+        addAndMakeVisible (colourPicker);
 
         writeConfigButton.setButtonText ("write");
         writeConfigButton.onClick = [&] () { trackerInterface.writeConfig(); };
@@ -115,34 +155,47 @@ public:
 
     void configurationReceived (juce::var config) override
     {
-        auto hue = config.getProperty ("hue", static_cast<float> (hueSlider.getValue()));
-        auto saturation = config.getProperty ("saturation", static_cast<float> (satSlider.getValue()));
-        auto brightness = config.getProperty ("brightness", static_cast<float> (brightSlider.getValue()));
+        // LED COLOR
+        auto currentColour = colourPicker.getColour();
+        auto hue = config.getProperty ("hue", static_cast<float> (currentColour.getHue()));
+        auto saturation = config.getProperty ("saturation", static_cast<float> (currentColour.getSaturation()));
+        auto brightness = config.getProperty ("brightness", static_cast<float> (currentColour.getBrightness()));
 
-        hueSlider.setValue (hue, juce::dontSendNotification);
-        satSlider.setValue (saturation, juce::dontSendNotification);
-        brightSlider.setValue (brightness, juce::dontSendNotification);
+        colourPicker.setHSV (float (hue) / 360.0f, saturation, brightness);
+        //
+
+        name = config.getProperty ("name", "");
+        repaint();
     }
 
     void paint (juce::Graphics& g) override
     {
         g.setColour (juce::Colours::cornflowerblue.withAlpha (0.1f));
         g.fillAll();
+
+        g.setColour (juce::Colours::white);
+
+        auto row = getLocalBounds().removeFromTop (25);
+        row.removeFromLeft (25 + 4);
+        g.setFont (20.0f);
+        g.drawText (name, row, juce::Justification::left);
     }
 
     void resized() override
     {
         auto bounds = getLocalBounds();
-        activity.setBounds (bounds.removeFromLeft (bounds.getHeight()));
-        bounds.removeFromLeft (10);
+        auto row = bounds.removeFromTop (25);
 
-        auto width = bounds.getWidth() / 4;
-        hueSlider.setBounds (bounds.removeFromLeft (width));
-        satSlider.setBounds (bounds.removeFromLeft (width));
-        brightSlider.setBounds (bounds.removeFromLeft (width));
-        writeConfigButton.setBounds (bounds.removeFromLeft (width));
+        activity.setBounds (row.removeFromLeft (25));
+        row.removeFromLeft (10);
+
+        bounds.removeFromTop (3);
+
+        row = bounds.removeFromTop (25);
+        colourPicker.setBounds (row.removeFromLeft (40));
+        row.removeFromLeft (50);
+        writeConfigButton.setBounds (row);
     }
-
 
     void timerCallback() override
     {
@@ -151,15 +204,13 @@ public:
     }
 
 
-
 private:
-    bool active = false;
-
     TrackerInterface& trackerInterface;
 
+    juce::String name;
+
+    bool active = false;
     ActivityComponent activity;
-    juce::Slider hueSlider;
-    juce::Slider satSlider;
-    juce::Slider brightSlider;
+    ColourPicker colourPicker;
     juce::TextButton writeConfigButton;
 };
