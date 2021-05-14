5df14db7a652ac62cb8477e2c09f763e3f3039a0
Daniel Rudrich <mail@danielrudrich.de>
2021-05-14 21:25:32 +0200

    adds sliders to visualizer

--- Source/destinations/Visualizer.h
+++ Source/destinations/Visualizer.h
@@ -5,7 +5,7 @@
 #include <JuceHeader.h>
 
 #include "Destination.h"
-
+#include "../tracker/RotationData.h"
 
 
 class Visualizer : public Destination
@@ -15,6 +15,34 @@ class Visualizer : public Destination
     public:
         Component (Visualizer& v) : Widget (v), visualizer (v)
         {
+            qw.setRange (-1.0, 1.0, 0.0);
+            qx.setRange (-1.0, 1.0, 0.0);
+            qy.setRange (-1.0, 1.0, 0.0);
+            qz.setRange (-1.0, 1.0, 0.0);
+
+            qw.setNumDecimalPlacesToDisplay (3);
+            qx.setNumDecimalPlacesToDisplay (3);
+            qy.setNumDecimalPlacesToDisplay (3);
+            qz.setNumDecimalPlacesToDisplay (3);
+
+            addAndMakeVisible (qw);
+            addAndMakeVisible (qx);
+            addAndMakeVisible (qy);
+            addAndMakeVisible (qz);
+
+            
+            yaw.setRange   (-180.0, 180.0, 0.0);
+            pitch.setRange (-180.0, 180.0, 0.0);
+            roll.setRange  (-180.0, 180.0, 0.0);
+
+            yaw.setNumDecimalPlacesToDisplay (1);
+            pitch.setNumDecimalPlacesToDisplay (1);
+            roll.setNumDecimalPlacesToDisplay (1);
+
+            addAndMakeVisible (yaw);
+            addAndMakeVisible (pitch);
+            addAndMakeVisible (roll);
+
             startTimer (50);
         }
 
@@ -25,31 +53,57 @@ class Visualizer : public Destination
             auto b = getLocalBounds();
             g.setColour (Colours::cornflowerblue);
             g.fillRoundedRectangle (b.toFloat(), 0.2 * b.getHeight());
-
-            g.setColour (Colours::white);
-
-            auto q = visualizer.getQuaternion();
-
-            auto text = String (q.w, 3) + " " +
-                        String (q.x, 3) + " " +
-                        String (q.y, 3) + " " +
-                        String (q.z, 3);
-            g.drawFittedText (text, b, Justification::centred, 1);
         }
 
+        void resized() override
+        {
+            auto bounds = getLocalBounds().reduced (5);
+
+            const auto half = (bounds.getWidth() - 8) / 2;
+
+            auto leftCol = bounds.removeFromLeft (half);
+            auto rightCol = bounds.removeFromLeft (half);
+
+            qw.setBounds (leftCol.removeFromTop (10));
+            qx.setBounds (leftCol.removeFromTop (10));
+            qy.setBounds (leftCol.removeFromTop (10));
+            qz.setBounds (leftCol.removeFromTop (10));
+
+            yaw.setBounds (rightCol.removeFromTop (10));
+            pitch.setBounds (rightCol.removeFromTop (10));
+            roll.setBounds (rightCol.removeFromTop (10));
+        }
 
         int getWidgetHeight() override
         {
-            return 40;
+            return 100;
         }
 
         void timerCallback() override
         {
-            repaint();
+            auto data = RotationData (visualizer.getQuaternion());
+
+            qw.setValue (data.qw);
+            qx.setValue (data.qx);
+            qy.setValue (data.qy);
+            qz.setValue (data.qz);
+
+            yaw.setValue (data.yawDegrees);
+            pitch.setValue (data.pitchDegrees);
+            roll.setValue (data.rollDegrees);
         }
 
     private:
         Visualizer& visualizer;
+
+        juce::Slider qw;
+        juce::Slider qx;
+        juce::Slider qy;
+        juce::Slider qz;
+
+        juce::Slider yaw;
+        juce::Slider pitch;
+        juce::Slider roll;
     };
 
 public:
@@ -57,7 +111,6 @@ public:
 
     Visualizer (TrackerInterface& source) : Destination (source)
     {
-
     }
 
     ~Visualizer() override {};
